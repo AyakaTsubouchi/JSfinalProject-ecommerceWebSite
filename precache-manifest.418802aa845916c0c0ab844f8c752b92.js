7de0a42c70e5207be70f41ec695a108376f8ae7d
Ayaka Tsubouchi <ayakawell@gmail.com>
2020-03-10 08:09:41 -0700

    Updates

---
+++ precache-manifest.418802aa845916c0c0ab844f8c752b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93749a3eafe931fbb71ecb848b240fa",
+    "url": "/JSfinalProject-ecommerceWebSite/index.html"
+  },
+  {
+    "revision": "20caa3508a721d82790a",
+    "url": "/JSfinalProject-ecommerceWebSite/static/css/main.f30df894.chunk.css"
+  },
+  {
+    "revision": "1eba20a67589ac32db6e",
+    "url": "/JSfinalProject-ecommerceWebSite/static/js/2.83d39196.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/JSfinalProject-ecommerceWebSite/static/js/2.83d39196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20caa3508a721d82790a",
+    "url": "/JSfinalProject-ecommerceWebSite/static/js/main.3f781596.chunk.js"
+  },
+  {
+    "revision": "1c784221d5687f8fa9d8",
+    "url": "/JSfinalProject-ecommerceWebSite/static/js/runtime-main.87370f1f.js"
+  }
+]);
